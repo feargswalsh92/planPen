48e85259977c72675d5aadd37bcbb1d0ecd098de
feargswalsh92 <feargswalsh@gmail.com>
2023-08-03 08:31:48 -0700

    redirect uri

--- main.py
+++ main.py
@@ -53,8 +53,9 @@ def authenticate_and_get_service() -> str:
                         "http://localhost:8080",
                         "http://localhost:8080/",
                         'https://plan-pen-6acfbe307ddb.herokuapp.com',
-                        'https://plan-pen-6acfbe307ddb.herokuapp.com'
-
+                        'https://plan-pen-6acfbe307ddb.herokuapp.com',
+                        'https://6481149ef84208702f5388ab89672ef9.preview.pluginlab.ai/',
+                        'https://6481149ef84208702f5388ab89672ef9.preview.pluginlab.ai"
                     ]
                 },
             }
